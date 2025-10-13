80ed4198f219b52296a1a699f99fd5c36c98651b
John Eric Ribo <ribojohneric@gmail.com>
2025-10-13 09:32:50 +0800

    Update Character_4.java

--- src/character/Character_4.java
+++ src/character/Character_4.java
@@ -1,1 +1,208 @@
+package character;
 
+public class Character_4 extends Miscellaneous implements CharacterStats{
+Miscellaneous misc = new Miscellaneous();
+
+    private String characterName;
+    private int health;
+    private int mana;
+    //attacks
+    //normal skill
+    private String normalSkillName;
+    private int normalSkillminRange;
+    private int normalSkillmaxRange;
+    private int manaGain;
+    //1
+    private String skillName1;
+    private int skill1minRange;
+    private int skill1maxRange;
+    private int manaCost1;
+    //2
+    private String skillName2;
+    private int skill2minRange;
+    private int skill2maxRange;
+    private int manaCost2;
+    //support skill
+    private String supportSkillName;
+    private double buff;
+    private double debuff;
+
+    //change for Character
+    @Override
+    public void setCharacter(){
+        setCharacterName("Ruby Galapad");
+        setCharacterStats(super.getBaseHealth(),super.getBaseMana()); //HP[1000] && Mana[250]
+        setCharacterNormalSkill("Normal", 75, 80, 25);
+        setCharacterSkill_1("Poussa", 130, 150, 150);
+        setCharacterSkill_2("Eagle pose", 175, 250, 250);
+        setCharacterSupportSkill("Tigas Ulo Tigas Katawan ",0.17, 0.10);
+    }
+    @Override
+    public void displayBackStory(){
+        System.out.println();
+        System.out.println("═════════════════════════════════════════════════════════════════════════════════");
+
+        System.out.println("("+getCharacterName()+") Backstory:");
+        System.out.println("A Male midnight ballerina dancer who does it for the passion for dancing, and his only main source of income. He created a viral dance move called “The Flying Eagle Dance.”. With his smooth movement despite his large size, he captures the attention of those around him. Just like those sudden rises of attention, he lost his way; he no longer possessed the humble person he was. As his greed grew, the entity took notice and was lured to the darkness, and was never seen again.");
+        
+        System.out.println("═════════════════════════════════════════════════════════════════════════════════");
+        System.out.println();
+    }
+
+    @Override
+    public void displayCharacterStats(){
+        System.out.println();
+        System.out.println("═════════════════════════════════════════════════════════════════════════════════");
+        System.out.println("                                     STATS: ");
+
+        displayCharacterNameHealthAndMana();
+
+        System.out.println("                                     SKILLS: ");
+        System.out.println("(" + this.normalSkillName+"): Damage: "+this.normalSkillminRange+"-"+this.normalSkillmaxRange+", Mana Gain: "+this.manaGain);
+        System.out.println("(" + this.skillName1+"): Damage: "+this.skill1minRange+"-"+this.skill1maxRange+", Mana Cost: "+this.manaCost1);
+        System.out.println("(" + this.skillName2+"): Damage: "+this.skill2minRange+"-"+this.skill2maxRange+", Mana Cost: "+this.manaCost2);
+        System.out.println("(" + this.supportSkillName+"): ???");
+
+        System.out.println("═════════════════════════════════════════════════════════════════════════════════");
+        System.out.println();
+    }
+    @Override
+    public void displayCharacterNameHealthAndMana(){
+        System.out.println();
+        System.out.println("═════════════════════════════════════════════════════════════════════════════════");
+        System.out.println("Name: " +getCharacterName());
+
+        misc.displayASCIIBars(getHealth());
+        System.out.println("\nHealth: ("+getHealth()+"/"+misc.getMaxHealth()+")\n");
+
+        misc.displayASCIIBars(getMana());
+        System.out.println("\nMana: ("+getMana()+"/"+super.getMaxMana()+")");
+
+        System.out.println("═════════════════════════════════════════════════════════════════════════════════");
+        System.out.println();
+    }
+
+    //setters
+    @Override
+    public void setCharacterName(String characterName){
+        this.characterName = characterName;
+    }
+    @Override
+    public void setCharacterStats(int health, int mana){
+        this.health = health;
+        this.mana = mana;
+    }
+    @Override
+    public void setCharacterNormalSkill(String skillName, int min, int max, int manaGain){
+        this.normalSkillName = skillName;
+        this.normalSkillminRange = min;
+        this.normalSkillmaxRange = max;
+        this.manaGain = manaGain;
+    }
+    @Override
+    public void setCharacterSkill_1(String skillName, int min,int max, int manaCost){
+        this.skillName1 = skillName;
+        this.skill1minRange = min;
+        this.skill1maxRange = max;
+        this.manaCost1 = manaCost;
+    }
+    @Override
+    public void setCharacterSkill_2(String skillName, int min,int max, int manaCost){
+        this.skillName2 = skillName;
+        this.skill2minRange = min;
+        this.skill2maxRange = max;
+        this.manaCost2 = manaCost;
+    }
+    @Override
+    public void setCharacterSupportSkill(String skillName,double buff, double debuff){
+         this.supportSkillName = skillName;
+         this.buff = buff;
+         this.debuff = debuff;
+    }
+
+    //gets
+    @Override
+    public String getCharacterName(){
+        return this.characterName;
+    }
+    @Override
+    public int getHealth(){
+        return this.health;
+    }
+    @Override
+    public int getMana(){
+        return this.mana;
+    }
+    @Override
+    public int getManaGain(){
+        return this.manaGain;
+    }
+    @Override
+    public int getManaCost1(){
+        return this.manaCost1;
+    }
+    @Override
+    public int getManaCost2(){
+        return this.manaCost2;
+    }
+    @Override
+    public int getNormalSkill(){
+        //addMana(manaGain);
+        //checkHealthAndManaIfBelowZero();
+        return random.nextInt((this.normalSkillminRange - this.normalSkillmaxRange) + 1) + this.normalSkillminRange;
+    }
+    @Override
+    public int getSkill_1(){
+        //minusMana(manaCost1);
+        //checkHealthAndManaIfBelowZero();
+        return random.nextInt((this.skill1maxRange - this.skill1minRange) + 1) + this.skill1minRange;
+    }
+    @Override
+    public int getSkill_2(){
+        //minusMana(manaCost2);
+        //checkHealthAndManaIfBelowZero();
+        return random.nextInt((this.skill2maxRange - this.skill2minRange) + 1) + this.skill2minRange;
+    }
+    @Override
+    public int getBuff(){
+        return (int)this.buff;
+    }
+    @Override
+    public int getDebuff(){
+        return (int)this.debuff;
+    }
+
+    //in battle
+    @Override
+    public void useSupportSkill(){
+
+    }
+    @Override
+    public void checkHealthAndManaIfBelowZero(){
+    
+    if(getHealth() < 0) this.health = 0;
+    
+    if(getMana() < 0) this.mana = 0;
+    
+    }
+    @Override
+    public void addHealth(int health){
+        System.out.println(" +"+health+" HP ");
+        this.health += health;
+    }
+    @Override
+    public void addMana(int mana){
+        System.out.println(" -"+mana+" Mana ");
+        this.mana += mana;
+    }
+    @Override
+    public void minusHealth(int damage){
+        System.out.println(" -"+damage+" HP ");
+        this.health -= damage;
+    }
+    @Override
+    public void minusMana(int manaCost){
+        System.out.println(" -"+manaCost+" Mana");
+        this.mana -= manaCost;
+    }
+}
