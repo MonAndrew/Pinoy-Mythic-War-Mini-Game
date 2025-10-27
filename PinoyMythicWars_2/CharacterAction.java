aceb6a634149d443f4eae15a48ac8d84ee99b641
CheonYeoWon <junwelconson65@gmail.com>
2025-10-27 16:47:06 +0800

    Sheeshh

---
+++ PinoyMythicWars_2/CharacterAction.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PinoyMythicWars_2;
+
+/**
+ *
+ * @author junwe
+ */
+public interface CharacterAction {
+    
+    public void attack(CharacterStatsBase target);
+    public void supportSkill();
+    public void burstSkill(CharacterStatsBase target);
+    public void takeDamage(int damage);
+    public boolean isAlive();
+    public void showStatus();
+    
+}
---
+++ PinoyMythicWars_2/Zaldiko.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PinoyMythicWars_2;
+
+/**
+ *
+ * @author junwe
+ */
+public class Zaldiko extends CharacterStatsBase{
+    
+    public Zaldiko(){
+        super("Zaldiko", 1500, 300);
+    }
+
+    @Override
+    public void attack(CharacterStatsBase target) {
+        System.out.println(name + " attacks " + target.getName() + " for " + attack + " damage!");
+        target.takeDamage(attack);
+    }
+    
+    @Override
+    public void supportSkill(){
+        int cost = 115;
+        int healAmount = 200;
+
+        currentHealth += healAmount;
+        
+        System.out.println(name + " used Ako si Zaldiko and healed for " + healAmount + " HP!" + "| Updated HP: " + currentHealth);
+        
+        
+    }
+    
+    @Override
+    public void burstSkill(CharacterStatsBase target){
+        int cost = 275;
+     
+        int damage = (int)(attack * 2.5);
+        System.out.println(name + " unleashes Final Judgement for " + damage + " damage!");
+        target.takeDamage(damage);
+    }
+}
