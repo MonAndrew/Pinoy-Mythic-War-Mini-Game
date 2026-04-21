425dc52d1735e0c027b50e468d9868e178006480
MonAndrew <ludovicemonandrew@gmail.com>
2026-04-21 12:01:37 +0800

    chore: Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+
 public class Main {
-    public static void main(String aa[]){
-        TurnBasedGame obj = new TurnBasedGame();
+    static void Game(){
+        TurnBasedGame game = new TurnBasedGame();
+    }
+
+    public static void main(String args[]){
+        Game();
     }
 }
 
